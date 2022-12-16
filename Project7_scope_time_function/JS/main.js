8c029d7b8a5cdfb661621f2aae9747501a0052e2
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2022-12-16 12:43:36 -0800

    Add files via upload

---
+++ Project7_scope_time_function/JS/main.js
@@ -0,0 +1,29 @@
+var x = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+    }
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_1() {
+    var X = 10;
+    console.log(15 + X);
+}
+function Add_numbers_2() {
+    console.log(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
