6cb3e2686729f41a54ce97bad3770f2e402182fa
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2022-12-21 15:17:26 -0800

    Update main.js

--- Project6_ternary operators_constructors/JS/main.js
+++ Project6_ternary operators_constructors/JS/main.js
@@ -1,12 +1,12 @@
 function Ride_Function() {
     var Height , Can_ride ;
-    Height = document.getElementByid("Height" ).value;
+    Height = document.getElementById("Height" ).value;
     Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
-    document.getElementByid("Ride").innerHTML = Can_ride + " to ride.";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
 function count_Function() {
-    document.getElementByid("Counting").innerHTML = Count();
+    document.getElementById("Counting").innerHTML = Count();
     function Count() {
         var Starting_point = 9;
         function Plus_one() {Starting_point += 1;}
