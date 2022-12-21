b6fbf80639e23d8f038d08e9b32e5734fe34aba1
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2022-12-20 18:57:07 -0800

    Update main.js

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -6,5 +6,5 @@ function my_Dictionary() {
     Age: 5 ,
     Sound: "Bark!"
     };
-    document.getElementByid("Dictionary").innerHTML= Animal.Sound;
+    document.getElementById("Dictionary").innerHTML= Animal.Sound;
 }
