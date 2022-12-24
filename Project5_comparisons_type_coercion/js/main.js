28fec538edc198617909a4615fd9c5ef5455208d
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2022-12-24 02:31:26 -0800

    Update main.js

--- Project5_comparisons_type_coercion/js/main.js
+++ Project5_comparisons_type_coercion/js/main.js
@@ -73,4 +73,4 @@ function not_Function() {
 
 document.write("\"Be who you are and say what you feel,"
 + " because those who mind don\'t matter and those who matter don\'t mind.\""
-+ "-Dr. Seuss");
++ "-Dr. Seuss" + "-March 2, 1904");
