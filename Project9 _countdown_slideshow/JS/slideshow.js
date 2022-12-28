ef006c21d0ed34145433271074bf427e70ea7b10
Muhammad-SAW-Stack <109488929+Muhammad-SAW-Stack@users.noreply.github.com>
2022-12-28 15:19:59 -0800

    Update slideshow.js

--- Project9 _countdown_slideshow/JS/slideshow.js
+++ Project9 _countdown_slideshow/JS/slideshow.js
@@ -1,3 +1,4 @@
+//The let keyword is used to declare variables in JavaScript
 let slideIndex = 0;
 showSlides();
 
@@ -12,15 +13,21 @@ function currentSlide(n) {
 }
 
 function showSlides() {
+    //The let keyword is used to declare variables in JavaScript
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    //for - loops through a block of code a number of times
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
+    //The increment ( ++ ) operator increments (adds one to) 
+    //its operand and returns the value before or after the increment
     slideIndex++;
+    //Use the if statement to specify a block of JavaScript code to be executed if a condition is true.
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    //The setTimeout () method allows you to execute a piece of code after a certain amount of time has passed
+    setTimeout(showSlides, 2000); // Change image every 2000 milliseconds 2 seconds
   }
 
 
